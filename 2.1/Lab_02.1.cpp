481822dcaeae908be3e05ee31237c3895d97d4e6
Chuha4308 <andrii.chukhrai.IK.2024@lpnu.ua>
2024-09-17 13:48:55 +0300

    main

---
+++ 2.1/Lab_02.1.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+#include<cmath> 
+using namespace std;
+int main()
+{
+	double a;
+	cout << "a = "; cin >> a;
+	double z1 = pow(((1 + a + pow(a, 2)) / (2 * a + pow(a, 2))) + 2 - ((1 - a + pow(a, 2)) / (2 * a - pow(a, 2))), -1) * (5 - 2 * pow(a, 2));
+	double z2 = (4 - pow(a, 2)) / 2.0;
+	cout << "z1: " << z1 << endl;
+	cout << "z2: " << z2 << endl;
+	return 0;
+}
